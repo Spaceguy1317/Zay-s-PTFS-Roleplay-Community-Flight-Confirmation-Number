d814243931fcc6169f6e0687d9a320b3578aedb9
Spaceguy1317 <bananasmonke420@gmail.com>
2024-06-28 13:34:38 +0530

    Update randomizerjava.js

--- randomizerjava.js
+++ randomizerjava.js
@@ -1,5 +1,5 @@
 function generateRandomString() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const charactersLength = characters.length;
     let result = '';
     for (let i = 0; i < 6; i++) {
